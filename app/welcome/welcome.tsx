ccc5a1cf6f1a376daf31878c0c2ea6f9658eee43
35018 <nhs35018@nhs.hal.ac.jp>
2026-05-23 12:13:57 +0900

    タイトル改修

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -2,14 +2,14 @@ import bg from "../data/1-back.png"
 
 export function Welcome() {
   return (
-    <main className="flex flex-col  items-center justify-center bg-[url(data/1-back.png)] h-150 w-full" >
-          <h1 className="text-2xl mb-3">title</h1>
-          <a href="/game/1">
-          <button className="bg-amber-400 rounded-2xl px-3 py-5">
-            <p className="font-bold text-amber-900">夏休み開始</p>
-            
-          </button>
-              </a>
+    <main className=" bg-[url(data/1-back.png)] h-screen w-full flex flex-col  items-center justify-center" >
+      <h1 className="text-2xl mb-3">ぼくのなつやすみver1.0</h1>
+      <a href="/game/1">
+      <button className="bg-amber-400 rounded-2xl px-3 py-5">
+      <p className="font-bold text-amber-900">夏休み開始</p>
+        </button>
+      </a>
+
     </main>
   );
 }
